52f88ea70b22f7aca0da6aefae2236c50a8c1b5b
Lucas Saenz <lukiacade05@gmail.com>
2026-06-16 01:47:47 -0300

    Se realiza rollback sobre el inciso 2.1

--- src/Catalogo/Producto.java
+++ src/Catalogo/Producto.java
@@ -1,46 +1,26 @@
 package Catalogo;
 
-
-
 public class Producto extends Item {
 
+	private int SKU;
+	private double precioFinal;
+	private String categoria;
+
+	public Producto(String nombre, String descripcion, double precio, double descuento, int peso, int SKU, String categoria) {
+		super(nombre, descripcion, precio, descuento, peso);
+		this.SKU = SKU;
+		this.precioFinal = precio * (1-descuento);
+		this.categoria = categoria;
+	}
 	
+	public double getPrecioFinal () {
+		return precioFinal;
+	}
 	
-    private Integer sku; 
-    private String categoria;
-
-    
-    
-    public Producto(String nombre, String descripcion, double precioBase, double descuento, Integer sku, String categoria) {
-        super(nombre, descripcion, precioBase, descuento);
-        this.sku = sku;
-        this.categoria = categoria;
+	/*
+	@Override
+    public boolean validarProducto() {
+        return true;
     }
-
-    
-    
-    @Override
-    public double getPrecioFinal() {
-        return this.getPrecioBase() * (1 - this.getDescuento());
-    }
-
-    
-    @Override
-    public boolean validar() {
-        boolean nombreValido = this.getNombre() != null;
-        boolean skuValido = this.sku != null && this.sku > 0;
-        boolean dinamicosValidos = !this.getAtributosDinamicos().containsValue(null);
-
-        return nombreValido && skuValido && dinamicosValidos;
-    }
-
-    
-    public Integer getSKU() {
-        return this.sku;
-    }
-
-    
-    public String getCategoria() {
-        return this.categoria;
-    }
+    */
 }
