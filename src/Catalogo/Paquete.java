f45f76a415cf1245b4d04eae76105147b6d1276a
Lucas Saenz <lukiacade05@gmail.com>
2026-06-16 21:49:13 -0300

    Se acomoda el inciso 2.1

--- src/Catalogo/Paquete.java
+++ src/Catalogo/Paquete.java
@@ -2,43 +2,26 @@ package Catalogo;
 
 import java.util.List;
 
-
-
 public class Paquete extends Item {
 
+private List<Item> items;
 	
+	public Paquete(String nombre, String descripcion, double precio, double descuento, int peso, List<Item> items) {
+		super(nombre, descripcion, precio, descuento, peso);
+		this.items = items;
+	}
 	
-    private List<Item> items;
-
-  
-    
-    public Paquete(String nombre, String descripcion, double precioBase, double peso, double descuento, List<Item> items) {
-        super(nombre, descripcion, precioBase, peso, descuento);
-        this.items = items;
+	public double getPrecioFinal () {
+		double total = items.stream().mapToDouble(item -> ((Producto) item).getPrecioFinal()).sum();
+		return (total * (1-descuento));
+	}
+	
+	@Override
+    public boolean validarProducto() {
+		boolean nombreValido = nombre != null;        
+        boolean itemsValidos = this.items.stream().allMatch(item -> item.validarProducto());                            
+        boolean dinamicosValidos = !this.getAttributes().containsValue(null);
+        return nombreValido && itemsValidos && dinamicosValidos;
     }
-
-    
-    
-    @Override
-    public double getPrecioFinal() {
-        double totalItems = this.items.stream()
-                                            .mapToDouble(item -> item.getPrecioFinal())
-                                            .sum();
-        
-        return totalItems * (1 - this.getDescuento());
-    }
-
-    
-    @Override
-    public boolean validar() {
-        boolean baseValida = super.validar();
-        boolean itemsValidos = this.items.stream().allMatch(item -> item.validar());
-
-        return baseValida && itemsValidos;
-    }
-
-    
-    public List<Item> getItems() {
-        return this.items;
-    }
+	
 }
