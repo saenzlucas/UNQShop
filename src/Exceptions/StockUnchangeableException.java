c025ce9492190f0426da436d79fe8fd3988db6fe
Lucas Saenz <lukiacade05@gmail.com>
2026-06-30 21:54:27 -0300

    Delete src/Exceptions/StockUnchangeableException.java

--- src/Exceptions/StockUnchangeableException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class StockUnchangeableException extends RuntimeException {
-	public StockUnchangeableException () {
-        super(String.format("No se puede usar modificar el stock"));
-    }
-}
